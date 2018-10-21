b7025788177741c6675c3b9704e815d35ad6c254
abhithakan <amthakan@wpi.edu>
2018-10-21 18:14:41 -0400

    added text raw input for user commands

--- experiment.py
+++ experiment.py
@@ -25,33 +25,81 @@ def main():
     sit_left = promp.ProMP("../scripts/DataICRA19/BaselineB")
     sit_front = promp.ProMP("../scripts/DataICRA19/BaselineB")
     sit_back = promp.ProMP("../scripts/DataICRA19/BaselineB")
+    
+    # take gripper command
+    # drop gripper command
 
+    text = raw_input("Start 1st case? (y/n)")
+    if text == 'n':
+        print "ok bye"
+    else:
+        face.look_right()
+        stand_right.test_promp()
+        # take command
+        stand_right.reset_right_hand()
+        # drop command
+        face.set_neutral()
+        base.move_right()
 
-    face.look_right()
-    stand_right.test_promp()
-    stand_right.reset_right_hand()
-    face.set_neutral()
+    text = raw_input("Start 2nd case? (y/n)")
+    if text == 'n':
+        print "ok bye"
+    else:
+        stand_front.test_promp()
+        # take command
+        stand_front.reset_right_hand()
+        # drop command
 
-    base.move_right()
-    stand_front.test_promp()
-    stand_front.reset_right_hand()
+    text = raw_input("Start 3rd case? (y/n)")
+    if text == 'n':
+        print "ok bye"
+    else:
+        stand_back.test_promp()
+        # take command
+        stand_back.reset_right_hand()
+        # drop command
 
-    stand_back.test_promp()
-    stand_back.reset_right_hand()
+    text = raw_input("Reset Robot? (y/n)")
+    if text == 'n':
+        print "ok bye"
+    else:
+        base.move_left()
+    
+    text = raw_input("Start 4th case? (y/n)")
+    if text == 'n':
+        print "ok bye"
+    else:
+        face.look_left()
+        sit_left.test_promp()
+        # take command
+        sit_left.reset_right_hand()
+        # drop command
+        face.set_neutral()
+        base.move_left()
+    
+    text = raw_input("Start 5th case? (y/n)")
+    if text == 'n':
+        print "ok bye"
+    else:
+        sit_front.test_promp()
+        # take command
+        sit_front.reset_right_hand()
+        # drop command
 
-    base.move_left()
-    face.look_left()
-    sit_left.test_promp()
-    sit_left.reset_right_hand()
-    face.set_neutral()
+    text = raw_input("Start 6th case? (y/n)")
+    if text == 'n':
+        print "ok bye"
+    else:
+        sit_back.test_promp()
+        # take command
+        sit_back.reset_right_hand()
+        # drop command
 
-    base.move_left()
-    sit_left.test_promp()
-    sit_left.reset_right_hand()
-
-    sit_left.test_promp()
-    sit_left.reset_right_hand()    
-    base.move_right()
+    text = raw_input("Reset Robot? (y/n)")
+    if text == 'n':
+        print "ok bye"
+    else:
+        base.move_right()
 
 if __name__ == '__main__':
     main()
