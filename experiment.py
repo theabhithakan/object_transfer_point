2f6e301086d0a8ab24247d4d1b7d1b71b149ecdf
abhithakan <amthakan@wpi.edu>
2018-10-21 20:53:09 -0400

    adding grip commands

--- experiment.py
+++ experiment.py
@@ -8,98 +8,134 @@ import baxter_interface
 import promp
 import move_base
 import head_wobbler
+from reflex_msgs.msg import PoseCommand 
 
 
 
 def main():
     rospy.init_node("OTP_Experiment", disable_signals=True)
-
+    print "init"
     face = head_wobbler.Wobbler()
     base = move_base.MoveBase()
+    print "initialized"
+    grip_pub = rospy.Publisher("/right_hand/command_position", PoseCommand, queue_size=10)
+    drop_pub = rospy.Publisher("/right_hand/command_position", PoseCommand, queue_size=10)
     # base.move_left()
     # base.move_right()
     # base.move_left()
-    stand_right = promp.ProMP("../scripts/DataICRA19/BaselineB")
-    stand_front = promp.ProMP("../scripts/DataICRA19/BaselineB")
-    stand_back = promp.ProMP("../scripts/DataICRA19/BaselineB")
-    sit_left = promp.ProMP("../scripts/DataICRA19/BaselineB")
-    sit_front = promp.ProMP("../scripts/DataICRA19/BaselineB")
-    sit_back = promp.ProMP("../scripts/DataICRA19/BaselineB")
+    # stand_right = promp.ProMP("../scripts/DataICRA19/BaselineB")
+    # stand_front = promp.ProMP("../scripts/DataICRA19/BaselineB")
+    # stand_back = promp.ProMP("../scripts/DataICRA19/BaselineB")
+    # sit_left = promp.ProMP("../scripts/DataICRA19/BaselineB")
+    # sit_front = promp.ProMP("../scripts/DataICRA19/BaselineB")
+    # sit_back = promp.ProMP("../scripts/DataICRA19/BaselineB")
     
-    # take gripper command
-    # drop gripper command
+    # # take gripper command
+    # # drop gripper command
 
-    text = raw_input("Start 1st case? (y/n)")
-    if text == 'n':
-        print "ok bye"
-    else:
-        face.look_right()
-        stand_right.test_promp()
-        # take command
-        stand_right.reset_right_hand()
-        # drop command
-        face.set_neutral()
-        base.move_right()
 
-    text = raw_input("Start 2nd case? (y/n)")
-    if text == 'n':
-        print "ok bye"
-    else:
-        stand_front.test_promp()
-        # take command
-        stand_front.reset_right_hand()
-        # drop command
+    # text = raw_input("Start 1st case? (y/n)")
+    # if text == 'n':
+    #     print "ok bye"
+    # else:
+    #     face.look_right()
+    #     stand_right.test_promp()
+    #     # take command
+    #     stand_right.reset_right_hand()
+    #     # drop command
+    #     face.set_neutral()
+    #     base.move_right()
 
-    text = raw_input("Start 3rd case? (y/n)")
-    if text == 'n':
-        print "ok bye"
-    else:
-        stand_back.test_promp()
-        # take command
-        stand_back.reset_right_hand()
-        # drop command
+    # text = raw_input("Start 2nd case? (y/n)")
+    # if text == 'n':
+    #     print "ok bye"
+    # else:
+    #     stand_front.test_promp()
+    #     # take command
+    #     stand_front.reset_right_hand()
+    #     # drop command
 
-    text = raw_input("Reset Robot? (y/n)")
-    if text == 'n':
-        print "ok bye"
-    else:
-        base.move_left()
+    # text = raw_input("Start 3rd case? (y/n)")
+    # if text == 'n':
+    #     print "ok bye"
+    # else:
+    #     stand_back.test_promp()
+    #     # take command
+    #     stand_back.reset_right_hand()
+    #     # drop command
+
+    # text = raw_input("Reset Robot? (y/n)")
+    # if text == 'n':
+    #     print "ok bye"
+    # else:
+    #     base.move_left()
     
-    text = raw_input("Start 4th case? (y/n)")
-    if text == 'n':
-        print "ok bye"
-    else:
-        face.look_left()
-        sit_left.test_promp()
-        # take command
-        sit_left.reset_right_hand()
-        # drop command
-        face.set_neutral()
-        base.move_left()
+    # text = raw_input("Start 4th case? (y/n)")
+    # if text == 'n':
+    #     print "ok bye"
+    # else:
+    #     face.look_left()
+    #     sit_left.test_promp()
+    #     # take command
+    #     sit_left.reset_right_hand()
+    #     # drop command
+    #     face.set_neutral()
+    #     base.move_left()
     
-    text = raw_input("Start 5th case? (y/n)")
-    if text == 'n':
-        print "ok bye"
-    else:
-        sit_front.test_promp()
-        # take command
-        sit_front.reset_right_hand()
-        # drop command
+    # text = raw_input("Start 5th case? (y/n)")
+    # if text == 'n':
+    #     print "ok bye"
+    # else:
+    #     sit_front.test_promp()
+    #     # take command
+    #     sit_front.reset_right_hand()
+    #     # drop command
 
-    text = raw_input("Start 6th case? (y/n)")
-    if text == 'n':
-        print "ok bye"
-    else:
-        sit_back.test_promp()
-        # take command
-        sit_back.reset_right_hand()
-        # drop command
+    # text = raw_input("Start 6th case? (y/n)")
+    # if text == 'n':
+    #     print "ok bye"
+    # else:
+    #     sit_back.test_promp()
+    #     # take command
+    #     sit_back.reset_right_hand()
+    #     # drop command
 
-    text = raw_input("Reset Robot? (y/n)")
-    if text == 'n':
-        print "ok bye"
-    else:
-        base.move_right()
+    # text = raw_input("Reset Robot? (y/n)")
+    # if text == 'n':
+    #     print "ok bye"
+    # else:
+    #     base.move_right()
+    print "starting"
+    grip = PoseCommand()
+    grip.f1 = 2.2
+    grip.f2 = 2.2
+    grip.f3 = 2.2
+    grip.preshape = 0.0
+    # grip.velocity.f1 = 1.0
+    # grip.velocity.f2 = 1.0
+    # grip.velocity.f3 = 1.0
+    # grip.velocity.preshape = 2.0
+    start = time.time()
+    stop = 4
+    while time.time()-start < stop:
+            grip_pub.publish(grip)
+
+    drop = PoseCommand()
+    drop.f1 = 0.0
+    drop.f2 = 0.0
+    drop.f3 = 0.0
+    drop.preshape = 0.0
+    # drop.velocity.f1 = 1.0
+    # drop.velocity.f2 = 1.0
+    # drop.velocity.f3 = 1.0
+    # drop.velocity.preshape = 2.0
+    start = time.time()
+    stop = 4
+    while time.time()-start < stop:
+            drop_pub.publish(drop)
+
+    # drop_pub.publish(drop)
+    print "done"
 
 if __name__ == '__main__':
     main()
--- move_base.py
+++ move_base.py
@@ -5,7 +5,7 @@ import rospy
 import sys, time, os
 import numpy as np
 from geometry_msgs.msg import Twist
-PI = 3.1415926535897
+# PI = 3.1415926535897
 # from handover.msg import skeleton
 # from std_msgs.msg import Float64MultiArray
 
@@ -19,52 +19,30 @@ class MoveBase:
 
 
     def move_right(self):
-        # start = time.time()
-        # # self.move_base = rospy.Publisher("/amp_wpi/twist_command", Twist, queue_size=10)
-        # base = Twist()
-        # base.angular.z = -0.2
-        # rate = 30
-        # r = rospy.Rate(rate)
-        # stop = 3.1
-        # while time.time()-start < stop:
-        #     self.move_base.publish(base)
-        #     r.sleep()
-
+        start = time.time()
+        # self.move_base = rospy.Publisher("/amp_wpi/twist_command", Twist, queue_size=10)
         base = Twist()
-        angular_speed = -0.2*PI/180
-        relative_angle = PI/2
-        base.linear.x = 0
-        base.linear.y = 0
-        base.linear.z = 0
-        base.angular.x = 0
-        base.angular.y = 0
-        base.angular.z = angular_speed
-
-        t0 = rospy.Time.now().to_sec()
-        current_angle = 0
-
-        while(current_angle < relative_angle):
+        base.angular.z = -0.2
+        rate = 30
+        r = rospy.Rate(rate)
+        stop = 3.1
+        while time.time()-start < stop:
             self.move_base.publish(base)
-            t1 = rospy.Time.now().to_sec()
-            current_angle = angular_speed*(t1-t0)
-
-        base.angular.z = 0
-        move_base.publish
-
+            r.sleep()
 
 
 
     def move_left(self):
-        # start = time.time()
-        # # self.move_base = rospy.Publisher("/amp_wpi/twist_command", Twist, queue_size=10)
-        # base = Twist()
-        # base.angular.z = 0.2
-        # rate = 30
-        # r = rospy.Rate(rate)
-        # stop = 3.1
-        # while time.time()-start < stop:
-        #     self.move_base.publish(base)
-        #     r.sleep()
+        start = time.time()
+        # self.move_base = rospy.Publisher("/amp_wpi/twist_command", Twist, queue_size=10)
+        base = Twist()
+        base.angular.z = 0.2
+        rate = 30
+        r = rospy.Rate(rate)
+        stop = 3.1
+        while time.time()-start < stop:
+            self.move_base.publish(base)
+            r.sleep()
 
 
 
